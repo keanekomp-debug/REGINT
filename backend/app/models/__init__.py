8c83540a5063ef0074fe0b4dd33b303c1046a471
keanekomp-debug <keanekomp@gmail.com>
2026-07-25 17:35:29 +0100

    Create __init__.py

---
+++ backend/app/models/__init__.py
@@ -0,0 +1,8 @@
+"""Database models"""
+from app.models.publications import Publication
+from app.models.companies import Company
+from app.models.manufacturers import Manufacturer
+from app.models.products import Product
+from app.models.edges import Edge
+from app.models.jobs import Job
+from app.models.logs import Log
